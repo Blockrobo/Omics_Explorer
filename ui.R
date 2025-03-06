5c2ac143787526679b3ef9b9ad29a0871ba5cc87
Cyberra <94319810+Blockrobo@users.noreply.github.com>
2025-03-06 15:58:04 +0100

    Added a DNA Visualization on the homepage

--- ui.R
+++ ui.R
@@ -11,20 +11,32 @@ ui <- navbarPage(
   # Home Tab
   tabPanel("Home",
            fluidPage(
-             titlePanel("Welcome to Omics Explorer"),
-             p("This web application allows you to analyze multi-omics datasets, visualize key insights, and explore biological relationships."),
-             p("You can:"),
-             tags$ul(
-               tags$li("View dataset summaries"),
-               tags$li("Perform enrichment analysis"),
-               tags$li("Conduct data-driven exploration"),
-               tags$li("Visualize data with an interactive genome browser")
+             fluidRow(
+               # Left Column: Text Content
+               column(8,
+                      titlePanel("Welcome to Omics Explorer"),
+                      p("This web application allows you to analyze multi-omics datasets, visualize key insights, and explore biological relationships."),
+                      p("You can:"),
+                      tags$ul(
+                        tags$li("View dataset summaries"),
+                        tags$li("Perform enrichment analysis"),
+                        tags$li("Conduct data-driven exploration"),
+                        tags$li("Visualize data with an interactive genome browser")
+                      )
+               ),
+               
+               # Right Column: GIF
+               column(3, align = "right",
+                      tags$img(src = "video-dna-vertical-unscreen.gif", 
+                               width = "100%", 
+                               style = "max-width: 600px; margin-top: 30px;")
+               )
              )
            )
   ),
   
   # Dataset Overview Tab
-  tabPanel("Dataset Overview",
+  tabPanel("Dataset Overview", h3("Datasets Overview"),
            sidebarLayout(
              sidebarPanel(
                selectInput("dataset", "Select Dataset:", choices = names(datasets))
@@ -36,11 +48,12 @@ ui <- navbarPage(
   ),
   
   # Enrichment Analysis Tab
-  tabPanel("Enrichment Analysis", h3("Enrichment Analysis Placeholder")),
+  tabPanel("Enrichment Analysis", h3("Enrichment Analysis")),
   
   # Data Analysis Tab
-  tabPanel("Data Analysis", h3("Data Analysis Placeholder")),
+  tabPanel("Data Analysis", h3("Data Analysis")),
   
   # Genome Browser Tab
-  tabPanel("Genome Browser", h3("Genome Browser Placeholder"))
+  tabPanel("Genome Browser", h3("Genome Browser"))
 )
+
