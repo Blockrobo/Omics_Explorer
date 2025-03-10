b93b02a9593ba72fe038181df4d19fb92bf33578
Cyberra <94319810+Blockrobo@users.noreply.github.com>
2025-03-10 11:50:49 +0100

    Refactored app structure: Added modular logic files for dataset management, clustering, data analysis (histogram, scatter plot, heatmap, boxplot, correlation), enrichment analysis, and genome browser. Connected all logic files to server.R.

--- server.R
+++ server.R
@@ -1,6 +1,20 @@
 # server.R
 source("global.R")
 
+#Logic Modules:
+source("modules/dataset_logic.R")
+source("modules/clustering_logic.R")
+source("modules/enrichment_analysis_logic.R")
+source("modules/genome_browser_logic.R")
+
+#Data Analysis Page Modules:
+source("modules/data_analysis/data_analysis_logic.R")
+source("modules/data_analysis/histogram_logic.R")
+source("modules/data_analysis/scatter_logic.R")
+source("modules/data_analysis/heatmap_logic.R")
+source("modules/data_analysis/boxplot_logic.R")
+source("modules/data_analysis/correlation_logic.R")
+
 server <- function(input, output, session) {
   
   # Reactive dataset based on selection
